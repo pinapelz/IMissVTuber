607674f65fe545f6b5568a4392284a21937b4996
Pinapelz <yukais@pinapelz.com>
2023-10-29 02:55:43 -0700

    Move footer component - move to directly coded into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import './styles/App.css'
-import Footer from "./components/Footer";
-import LandingPage from './pages/LandingPage'
+import "./styles/App.css";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
-    </BrowserRouter>
-    <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
+      <footer>
+        <a href="https://github.com/pinapelz/imisssomeone">Source Code</a>
+        <p>Not affiliated with Phase Connect or Erina Makina</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
