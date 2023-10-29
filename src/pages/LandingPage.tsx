97d8a40e30916a8e469a1267b99a6b7480507d89
Pinapelz <yukais@pinapelz.com>
2023-10-28 18:24:28 -0700

    fix: typescript build

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import CurrentStatus from "../components/CurrentStatus";
 import Footer from "../components/Footer";
 import ProgressBar from "../components/ProgressBar";
