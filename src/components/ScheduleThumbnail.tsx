8f4a8cef3dd35429290506ea5e5b0abbab9355a3
Pinapelz <yukais@pinapelz.com>
2023-11-10 11:55:38 -0800

    add modal for scheduled thumbnail on desktop

--- src/components/ScheduleThumbnail.tsx
+++ src/components/ScheduleThumbnail.tsx
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import React, {useEffect, useState} from 'react';
+import Modal from 'react-modal';
 
-
+Modal.setAppElement('#root')
 
 const ScheduleThumbnail = 
 React.memo(function ScheduleThumbnail() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [thumbnail, setThumbnail] = useState("");
+    const [modalIsOpen, setModalIsOpen] = useState(false);
 
     useEffect(() => {
         axios.get('https://imisserinya.vercel.app/api/schedule')
@@ -23,6 +25,19 @@ React.memo(function ScheduleThumbnail() {
             })
     }, [])
 
+    function openModal() {
+        if (window.innerWidth <= 768) {
+            window.open(thumbnail, '_blank');
+        } else {
+            setModalIsOpen(true);
+        }
+    }
+
+    function closeModal() {
+        setModalIsOpen(false);
+    }
+
+
     if(loading){
         return <p>Loading...</p>
     }
@@ -34,7 +49,31 @@ React.memo(function ScheduleThumbnail() {
         <>
         <h1>Weekly Schedule</h1>
         <p>Let's hope Erina updated her schedule this week...</p>
-        <img src={thumbnail} style={{ maxWidth: '100%', height: 'auto' }} alt="Schedule Thumbnail"/>
+        <img src={thumbnail} style={{ maxWidth: '100%', height: 'auto' }} alt="Schedule Thumbnail" onClick={openModal}/>
+        <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Example Modal"
+            style={{
+                overlay: {
+                    backgroundColor: 'rgba(0, 0, 0, 0.75)', 
+                },
+                content: {
+                    top: '50%',
+                    left: '50%',
+                    right: 'auto',
+                    bottom: 'auto',
+                    transform: 'translate(-50%, -50%)',
+                    width: '80%', 
+                    height: '80%',
+                    maxWidth: '100%',
+                    maxHeight: '100%',
+                    overflow: 'auto',
+                },
+            }}
+        >
+            <img src={thumbnail} style={{ width: '100%', height: 'auto' }} alt="Schedule Thumbnail" />
+        </Modal>
         </>
     )
 });
