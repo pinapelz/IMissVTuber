9e26262d99838175014e0fdda966a594ca56ece0
Pinapelz <yukais@pinapelz.com>
2024-06-08 02:11:18 -0700

    remove schedule waiting room from upcoming grid

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -34,32 +34,37 @@ export default function SchedulePage() {
         />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-8 px-2">
-        {upcomingVideoData
-          ?.sort(
-            (a, b) =>
-              new Date(a.scheduled_time).getTime() -
-              new Date(b.scheduled_time).getTime()
-          )
-          .map((video) => (
-            <div
-              className="transform transition duration-500 ease-in-out hover:scale-105"
-              key={video.video_id}
-            >
-              <VideoCard
-                thumbnail={`https://img.youtube.com/vi/${video.video_id}/maxresdefault.jpg`}
-                title={video.title}
-                startTime={new Intl.DateTimeFormat("en-US", {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                  hour: "numeric",
-                  minute: "numeric",
-                  second: "numeric",
-                  timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
-                }).format(new Date(video.scheduled_time))}
-              />
-            </div>
-          ))}
+        {upcomingVideoData && upcomingVideoData.length > 0 ? (
+          upcomingVideoData
+            .filter(video => video.video_id !== process.env.NEXT_PUBLIC_SCHEDULE_THUMBNAIL)
+            .sort(
+              (a, b) =>
+                new Date(a.scheduled_time).getTime() -
+                new Date(b.scheduled_time).getTime()
+            )
+            .map((video) => (
+              <div
+                className="transform transition duration-500 ease-in-out hover:scale-105"
+                key={video.video_id}
+              >
+                <VideoCard
+                  thumbnail={`https://img.youtube.com/vi/${video.video_id}/maxresdefault.jpg`}
+                  title={video.title}
+                  startTime={new Intl.DateTimeFormat("en-US", {
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                    hour: "numeric",
+                    minute: "numeric",
+                    second: "numeric",
+                    timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+                  }).format(new Date(video.scheduled_time))}
+                />
+              </div>
+            ))
+        ) : (
+          <p>No waiting rooms</p>
+        )}
       </div>
       <div className="mt-4 text-lg">
         <p>{Intl.DateTimeFormat().resolvedOptions().timeZone}</p>
