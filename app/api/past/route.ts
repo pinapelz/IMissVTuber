7991edf45a113423a4ff3777e68142833f6fde46
Pinapelz <yukais@pinapelz.com>
2024-08-21 22:06:47 -0700

    force-no-store on /past api route

--- app/api/past/route.ts
+++ app/api/past/route.ts
@@ -1,5 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { HolodexApiClient, ChannelVideosParam } from 'holodex.js';
+
+export const fetchCache = 'force-no-store';
+export const revalidate = 0
 
 export async function GET() {
   const client = new HolodexApiClient({
@@ -8,19 +11,27 @@ export async function GET() {
   const videos = await client.getVideosByChannelId(process.env.CHANNEL_ID || "UCupmjRr7kPgzXKh-cPxxGbg", undefined, { limit: 10 } as ChannelVideosParam);
   let past_videos = [];
   for (let i = 0; i < videos.length; i++) {
-    if(videos[i].status == "past"){
+    if (videos[i].status == "past") {
       past_videos.push({
         title: videos[i].title,
         type: videos[i].videoType,
         scheduled_time: videos[i].availableAt,
         channel_name: videos[i].channel.englishName || videos[i].channel.name,
         video_id: videos[i].videoId,
-      });
+      },
+      );
     }
   }
-    past_videos.reverse();
-    return NextResponse.json({
+  past_videos.reverse();
+  return NextResponse.json({
     past_videos: past_videos,
-  });
+  },
+    {
+      headers: {
+        'Cache-Control': 'no-store',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+      }
+    });
 }
 
