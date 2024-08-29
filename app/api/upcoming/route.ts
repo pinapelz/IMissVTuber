f814fe0710c13d955560e107c78ebd9375a2eaa4
Pinapelz <yukais@pinapelz.com>
2024-08-29 15:54:52 -0700

    add force-no-store to upcoming api route

--- app/api/upcoming/route.ts
+++ app/api/upcoming/route.ts
@@ -1,5 +1,8 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { HolodexApiClient, ChannelVideosParam } from 'holodex.js';
+
+export const fetchCache = 'force-no-store';
+export const revalidate = 0
 
 export async function GET() {
   const client = new HolodexApiClient({
@@ -8,7 +11,7 @@ export async function GET() {
   const videos = await client.getVideosByChannelId(process.env.CHANNEL_ID || "UCupmjRr7kPgzXKh-cPxxGbg", undefined, { limit: 10 } as ChannelVideosParam);
   let scheduled_videos = [];
   for (let i = 0; i < videos.length; i++) {
-    if(videos[i].status == "upcoming"){
+    if (videos[i].status == "upcoming") {
       scheduled_videos.push({
         title: videos[i].title,
         type: videos[i].videoType,
@@ -20,6 +23,13 @@ export async function GET() {
   }
   return NextResponse.json({
     scheduled_videos: scheduled_videos,
-  });
+  },
+    {
+      headers: {
+        'Cache-Control': 'no-store',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+      }
+    });
 }
 
